af53b844c65adf7f3f5743c45b29e5abd6b6e754
Chris Smith <42@chrisryansmith.com>
2017-07-12 14:42:47 -0600

    add redirect and flash warning for users who are already logged in so they can't try to sign in again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
 
+  before_action :require_no_user, only: [:new]
+
+  def new
+  end
+
   def show
   end
 
+  private
+    def require_no_user
+      redirect_to user_path(current_user), warning: "You are already logged in.  Logout first to create a new account." if current_user
+    end
 end
