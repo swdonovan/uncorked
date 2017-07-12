537a37fbecd71b6e80b8cc64ecdbe4f3a40d2fca
MasonHolland <masontholland@gmail.com>
2017-07-12 17:22:46 -0600

    user_spec written

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,75 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+      it { should have_secure_password }
+      it { should validate_presence_of(:first_name) }
+      it { should validate_presence_of(:last_name) }
+      it { should validate_presence_of(:email) }
+      it { should validate_presence_of(:username) }
+      it { should validate_uniqueness_of(:username) }
+      it { should validate_presence_of(:phone_number) }
+    end
+
+    context "valid attributes" do
+      it "is valid with a first name, last name, username, email, phone number and password" do
+        user = User.new(
+                        first_name: "Mason",
+                        last_name: "Tom",
+                        email: "mason@tom.com",
+                        username: "ToMas",
+                        phone_number: "303-111-2222",
+                        password: "Password"
+                        )
+
+        expect(user).to be_valid
+      end
+    end
+  end
+
+  context "relationships" do
+    xit "belongs to ..." do
+      user = User.new(
+                      first_name: "Mason",
+                      last_name: "Tom",
+                      email: "mason@tom.com",
+                      username: "ToMas",
+                      phone_number: "303-111-2222",
+                      password: "Password"
+                      )
+
+      expect(user).to resond_to(:model)
+    end
+
+    xit "has many ..." do
+      user = User.new(
+                      first_name: "Mason",
+                      last_name: "Tom",
+                      email: "mason@tom.com",
+                      username: "ToMas",
+                      phone_number: "303-111-2222",
+                      password: "Password"
+                      )
+
+      expect(user).to resond_to(:model)
+    end
+  end
+
+  context "Factory" do
+    it "should be a user" do
+      expect(create(:user)).to be_a(User)
+    end
+
+    it "should have attributes" do
+      user = create(:user)
+      expect(user.first_name).to be_a(String)
+      expect(user.last_name).to be_a(String)
+      expect(user.username).to be_a(String)
+      expect(user.email).to be_a(String)
+      expect(user.bio).to be_a(String)
+      expect(user.phone_number).to be_a(String)
+      expect(user.password).to be_a(String)
+    end
+  end
 end
