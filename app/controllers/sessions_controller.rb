db6ecc0edad5a1369a88d959faf50a2e27712250
Thomas Soucie <tsoucie1858@gmail.com>
2017-07-12 14:52:40 -0600

    User Login Complete
    User can successfully login
    Happy (correct login information) and Sad (incorrect login information) paths

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:session][:username])
-    # binding.pry
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to user_path(current_user)
     else
-      flash[:error] = "LOG IN DUMMY"
-      # if @user.admin?
-      #   flash[:success] = "Admin Logged in as #{@user.email}"
-      #   redirect_to admin_dashboard_path
-      # else
-      #   flash[:success] = "Logged in as #{@user.email}"
-      #   redirect_to dashboard_path
+      flash[:error] = "Incorrect Login information, try again"
+      render :new
     end
   end
 
