8c5df7c45c88408c3e005d9c70b5c37c9b6af1bc
MasonHolland <masontholland@gmail.com>
2017-07-11 19:28:08 -0600

    started user profile spec wrote feature test for viewing profile when already logged in created migration for user generated User model

---
+++ db/migrate/20170712005542_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :email
+      t.text :bio
+      t.string :phone_number
+      t.string :password_digest
+      t.integer :role, default: 0
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/features/users/user_can_view_profile_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "user can view profile" do
+  context "when logged in" do
+    let(:user) { create(:user) }
+    it "user sees their profile" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit '/'
+
+      expect(page).to have_content("Hello #{user.first_name} #{user.last_name}")
+      expect(page).to have_content("Logout")
+      expect(page).not_to have_content("Login")
+
+      click_on "#{user.first_name} #{user.last_name}"
+
+      expect(current_path).to eq(user_path(user))
+      expect(page).to have_content(user.email)
+      expect(page).to have_content(user.bio)
+      expect(page).to have_content("Your Followers")
+      expect(page).to have_content("Following")
+    end
+  end
+
+  context "when not logged in" do
+    it "user is redirected to login"
+
+  end
+end
