8a5d801369edaec19fc3e68a152b245c207a6f73
Chris Smith <42@chrisryansmith.com>
2017-07-12 18:12:58 -0600

    add venue index page with links to venue show page
    [Finishes #148704911]

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get "/logout", to: "sessions#destroy"
 
   resources :users, only: [:show, :create, :new, :edit, :update]
-  resources :venues, only: [:index]
+  resources :venues, only: [:index, :show]
 end
