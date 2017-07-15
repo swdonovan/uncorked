5db583002fe2d87cf19a810acdfa4107e661de14
Thomas Soucie <tsoucie1858@gmail.com>
2017-07-15 12:12:50 -0600

    create outline for user_wants_to_follow_venue outline

---
+++ spec/features/users/user_wants_to_follow_venue_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "user wants to follow a venue" do
+  context "when logged in as a member" do
+    xit "member can follow a venue" do
+
+    end
+  end
+  context "when not logged in" do
+    xit "guest cannot see venue follow link" do
+
+    end
+    xit "guest is redirected back to homepage with a message" do
+
+    end
+  end
+end
