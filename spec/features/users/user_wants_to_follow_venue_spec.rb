27c80fc7e1a95441aa8b4f713e92d51596995596
Thomas Soucie <tsoucie1858@gmail.com>
2017-07-15 14:26:54 -0600

    implement bang method on FFG user_wants_to_follow_venue_spec

--- spec/features/users/user_wants_to_follow_venue_spec.rb
+++ spec/features/users/user_wants_to_follow_venue_spec.rb
@@ -1,23 +1,12 @@
 require 'rails_helper'
-=begin
-As a registered user that is logged in
-And I want to follow to a venue that is in the db
-When I visit the venue index page
-And I search for a venue
-I should see the individual venue show page
-And a link to follow
-And when I click the link to follow
-I should see a successful message
-And I should be following the venue
-=end
 
 RSpec.feature "user wants to follow a venue" do
   context "when logged in as a member" do
-    let(:user) { create(:user) }
-    let(:venue) { create(:venue) }
+    let!(:user) { create(:user) }
+    let!(:venue) { create(:venue) }
     it "member can follow a venue" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
+      # venue = create(:venue)
       visit venues_path
       expect(current_path).to eq("/venues")
       expect(page).to have_content(venue.name)
