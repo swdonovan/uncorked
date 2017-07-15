c457717cad5d46e5db71a8481f1137e6b23676e0
Chris Smith <42@chrisryansmith.com>
2017-07-15 12:28:16 -0600

    add wine show page

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,5 +1,16 @@
 class WinesController < ApplicationController
+
+  before_action :set_wine, only: [:show]
+
   def index
     @wines = Wine.paginate(:page => params[:page], :per_page => 30)
   end
+
+  def show
+  end
+
+  private
+    def set_wine
+      @wine = Wine.find(params[:id])
+    end
 end
---
+++ spec/features/wines/user_can_view_one_wine_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature 'User can see one wine' do
+  let!(:wines) { create_list(:wine, 2) }
+  context 'without being logged in' do
+    it 'guest can view a single wine\'s details' do
+      visit root_path
+
+      within '.right' do
+        click_on 'Wines'
+      end
+
+      expect(current_path).to eq wines_path
+
+      click_on wines.first.name
+
+      expect(current_path).to eq wine_path(wines.first)
+      expect(page).to have_content wines.first.name
+      expect(page).to have_content(wines.first.varietal)
+      expect(page).to have_content(wines.first.vintage)
+      expect(page).to have_content(wines.first.vineyard)
+      expect(page).to have_content(wines.first.rating)
+      expect(page).to have_content(wines.first.description)
+      expect(page).not_to have_content(wines.last.name)
+    end
+  end
+end
