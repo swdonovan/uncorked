9f4aa17d0d738760cab05e25cd17afd9eef9092c
Thomas Soucie <tsoucie1858@gmail.com>
2017-07-17 15:38:44 -0600

    remove new action from UserFollows controller and update link in the show page

--- app/controllers/user_follows_controller.rb
+++ app/controllers/user_follows_controller.rb
@@ -1,15 +1,16 @@
 class UserFollowsController < ApplicationController
-  def new
-    @user_follow = UserFollow.new
-  end
+  # def new
+  #   @user_follow = UserFollow.new
+  # end
 
   def create
-    binding.pry
+    # binding.pry
     # @follow = Venue.find(params["format"])
+    @user_follow = UserFollow.new
     @user_follow.user_id = current_user.id
     @user_follow.followable_type = params["followable"]
     @user_follow.followable_id = params["format"]
-    binding.pry
+    # binding.pry
     if @user_follow.save
       redirect_to user_venues_path(current_user), success: "Venue successfully followed!"
     else
