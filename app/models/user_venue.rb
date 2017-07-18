244a2ce7eb7fd8dc550630791eb580675de7ae67
MasonHolland <masontholland@gmail.com>
2017-07-17 23:14:55 -0600

    removed validations on join tables

--- app/models/user_venue.rb
+++ app/models/user_venue.rb
@@ -1,6 +1,4 @@
 class UserVenue < ApplicationRecord
-  validates_presence_of :user_id, :venue_id
-
   belongs_to :manager, polymorphic: true
   belongs_to :venue
 end
--- app/models/venue_wine.rb
+++ app/models/venue_wine.rb
@@ -1,6 +1,4 @@
 class VenueWine < ApplicationRecord
-  validates_presence_of :venue_id, :wine_id
-
   belongs_to :wine
   belongs_to :venue
 end
