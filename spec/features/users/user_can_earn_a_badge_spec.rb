194136914c06c8be36f966a02e1cabd46a5f2dec
MasonHolland <masontholland@gmail.com>
2017-07-19 16:12:38 -0600

    created test file

---
+++ spec/features/users/user_can_earn_a_badge_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.feature "User can earn a badge" do
+  context "when logged in" do
+    it "after completing 10 reviews you earn a badge" do
+      
+    end
+  end
+end
