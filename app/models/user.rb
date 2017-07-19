cfdd6902476575e430c08d211664556bafea72df
Thomas Soucie <tsoucie1858@gmail.com>
2017-07-19 08:29:34 -0600

    removed user has_many :reviews, as: :reviewable

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :user_venues, as: :manager
   has_many :venues, through: :user_venues
 
-  has_many :reviews, as: :reviewable
+  has_many :reviews
 
   def manager_has_venues_with_wine?(wine)
     !(venues & wine.venues).empty?
