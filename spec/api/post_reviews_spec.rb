a6c5a1d3562627ec22615166f6ebffaf4a0362f4
Thomas Soucie <tsoucie1858@gmail.com>
2017-07-19 22:16:09 -0600

    spec/api/post_reviews_spec setup

--- spec/api/post_reviews_spec.rb
+++ spec/api/post_reviews_spec.rb
@@ -1,45 +1,65 @@
 require 'rails_helper'
-=begin
-As a user
-When I navigate to the wines or venues #show page
-I should see a button/link to create API review
-And when I click on "Create API Review"
-I'll be taken to a new_api_review form
-And when I fill in review details (description & rating)
-And I click_on submit
-I should see a successful message
-And I should see all the reviews for that objects (wine/venue#show)
-=end
+
 describe "POST /api/v1/reviews" do
   context "POST a wine review" do
-    let(:user) { create{:user) }
-    let(:wine) { create(:wine) }
     it "posts review via API" do
+      user = create(:user)
+      wine = create(:wine)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit wine_path(wine)
+      get "/wines/#{wine.id}"
 
       expect(page).to have_link("Create Review via API", href: new_api_v1_review)
 
       click_link "Create Review via API"
 
       expect(current_path).to eq(new_api_v1_review)
-      expect(page).to have_link()
 
       fill_in "Description", with: "Had better"
       fill_in "Rating", with: 4
       click_button "Create Review"
-      
 
+      expect(response).to eq(201)
+      expect(response.body).to eq()
     end
-
-
-
   end
+
   context "POST a venue review" do
-    it "posts review via API" do
+    xit "posts review via API" do
+      user = create(:user)
+      venue = create(:venue)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
+      get "/venues/#{venue.id}"
+
+      expect(page).to have_link("Create Review via API", href: new_api_v1_review)
+
+      click_link "Create Review via API"
+
+      expect(current_path).to eq(new_api_v1_review)
+
+      fill_in "Description", with: "Had better"
+      fill_in "Rating", with: 4
+      click_button "Create Review"
+
+      expect(response).to eq(201)
+      expect(response.body).to eq()
     end
   end
 
+  context "when not logged in" do
+    xit "user cannot review venue via API" do
+      venue = create(:venue)
+      visit venue_path(venue)
+
+      expect(page).not_to have_content("Review")
+    end
+
+    xit "user cannot review wine via API" do
+      wine = create(:wine)
+      visit wine_path(wine)
+
+      expect(page).not_to have_content("Review")
+    end
+  end
 end
