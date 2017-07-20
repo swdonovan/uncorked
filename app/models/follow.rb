a1129f449aa872106c5c86beafb000ef4c21f174
Chris Smith <42@chrisryansmith.com>
2017-07-19 23:43:25 -0600

    refactor StreamRails into feed PORO

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,40 +2,27 @@ class Follow < ApplicationRecord
   belongs_to :target, polymorphic: true
   belongs_to :user
 
-  attr_reader :user_feed
-
   def target_feed_name
     target_type.downcase
   end
 
-  def user_feed
-    @user_feed ||= StreamRails.client.feed('user', user_id)
-  end
-
-  def report_activity(data)
-    user_feed.add_activity(data)
-  end
-
   def report_follow
-    user_feed.follow(target_feed_name, target_id)
-    report_activity( default_activitiy_data.merge( verb: 'follow' ) )
+    attrs = {
+      user_id: user_id,
+      target_type: target_type,
+      target_id: target_id,
+      target_feed: target_feed_name
+    }
+    Feed.new(attrs).follow
   end
 
   def report_unfollow
-    user_feed.unfollow(target_feed_name, target_id)
-    report_activity( default_activitiy_data.merge( verb: 'unfollow' ) )
+    attrs = {
+      user_id: user_id,
+      target_type: target_type,
+      target_id: target_id,
+      target_feed: target_feed_name
+    }
+    Feed.new(attrs).unfollow
   end
-
-  private
-
-
-    def default_activitiy_data
-      {
-        actor: "User:#{user_id}",
-        object: "#{target_type}:#{target_id}",
-        foreign_id: "#{target_type}:#{target_id}",
-        time: DateTime.now,
-        to: ["#{target_feed_name}:#{target_id}"]
-      }
-    end
 end
