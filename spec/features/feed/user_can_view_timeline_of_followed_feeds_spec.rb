d556cbc964a589cbacd6cb186259a8eb1e6d93c5
Chris Smith <42@chrisryansmith.com>
2017-07-19 19:49:44 -0600

    move user create for no activity spec to see if that clears travis

--- spec/features/feed/user_can_view_timeline_of_followed_feeds_spec.rb
+++ spec/features/feed/user_can_view_timeline_of_followed_feeds_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.feature "user wants to view timeline of followed feeds" do
 
   context 'when logged in as a member' do
-    let(:user) { create(:user) }
     context 'when there is no activity' do
+      let(:user) { create(:user) }
       it 'the user sees that they have no activity' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
