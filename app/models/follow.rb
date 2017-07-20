d2ca7226f2f59776d7f7ca9ba53a2c86c8ce4cb5
Chris Smith <42@chrisryansmith.com>
2017-07-19 18:11:35 -0600

    dry up follow api call in follow model

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,26 +2,32 @@ class Follow < ApplicationRecord
   belongs_to :target, polymorphic: true
   belongs_to :user
 
+  attr_reader :user_feed
+
   def target_feed_name
     target_type.downcase
   end
 
+  def user_feed
+    @user_feed ||= StreamRails.client.feed('user', user_id)
+  end
+
+  def report_activity(data)
+    user_feed.add_activity(data)
+  end
+
   def report_follow
-    user_feed = StreamRails.client.feed('user', user_id)
     user_feed.follow(target_feed_name, target_id)
-    activity_data = default_activitiy_data.merge( verb: 'follow' )
-    activity_response = user_feed.add_activity(activity_data)
+    report_activity( default_activitiy_data.merge( verb: 'follow' ) )
   end
 
   def report_unfollow
-    user_feed = StreamRails.client.feed('user', user_id)
     user_feed.unfollow(target_feed_name, target_id)
-    activity_data = default_activitiy_data.merge( verb: 'unfollow' )
-    activity_response = user_feed.add_activity(activity_data)
+    report_activity( default_activitiy_data.merge( verb: 'unfollow' ) )
   end
 
   private
-    attr_reader :user_feed
+
 
     def default_activitiy_data
       {
