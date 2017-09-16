cfbf90362b50058eb6fd5e6eb01c33653e7f6dc9
Ellen Cooper <ellencooper74@gmail.com>
2017-09-16 11:52:10 -0600

    git status

---
+++ spec/features/users/admins/admin_can_change_role_of_user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Admin edits role of user" do
+  it "can change default user to manager or admin" do
+    admin = create(:user, role: "admin")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    user_1, user_2 = create_list(:user, 2)
+
+    visit '/'
+    expect(page).to have_content("Admin Dashboard")
+
+    click_on("Manager Users")
+    within(".user-#{user_1.id}") do
+      expect(page).to have_content("Member")
+      click_on "Make Manager"
+      expect(page).to have_content("Manager")
+      expect(page).to_not have_content("Member")
+      click_on "Make Admin"
+      expect(page).to have_content("Admin")
+      expect(page).to_not have_content("Manager")
+      click_on "Make Member"
+      expect(page).to have_content("Member")
+      expect(page).to_not have_content("Manager")
+      expect(page).to_not have_content("Admin")
+    end
+  end
+end
