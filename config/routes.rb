248793c3abd03329e7202d978a83476a997785b1
Ellen Cooper <ellencooper74@gmail.com>
2017-09-16 14:53:17 -0600

    adds routes and controller for admin wines

--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users, only: [:index, :update, :destroy]
     resources :venues, only: [:index, :destroy]
+    resources :wines, only: [:index, :destroy]
     get '/dashboard', to: 'admin#show'
   end
 
