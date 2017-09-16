47f2d7545f60c56d0135b7369edf3d82cc388704
Ellen Cooper <ellencooper74@gmail.com>
2017-09-16 13:54:41 -0600

    modifies test and admin user table classes

--- spec/features/users/admins/admin_can_change_role_of_user_spec.rb
+++ spec/features/users/admins/admin_can_change_role_of_user_spec.rb
@@ -4,24 +4,30 @@ RSpec.feature "Admin edits role of user" do
   it "can change default user to manager or admin" do
     admin = create(:user, role: "admin")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-    user_1, user_2 = create_list(:user, 2)
+    user_1 = create(:user)
 
     visit '/'
     expect(page).to have_content("Admin Dashboard")
 
     click_on("Manage Users")
     within(".user-#{user_1.id}") do
-      expect(page).to have_content("Member")
+      within(".role") {expect(page).to have_content("member")}
       click_on "Make Manager"
-      expect(page).to have_content("Manager")
-      expect(page).to_not have_content("Member")
+    end
+    within(".user-#{user_1.id}") do
+      within(".role") {expect(page).to have_content("manager")}
+      within(".role") {expect(page).to_not have_content("member")}
       click_on "Make Admin"
-      expect(page).to have_content("Admin")
-      expect(page).to_not have_content("Manager")
+    end
+    within(".user-#{user_1.id}") do
+      within(".role") {expect(page).to have_content("admin")}
+      within(".role") {expect(page).to_not have_content("manager")}
       click_on "Make Member"
-      expect(page).to have_content("Member")
-      expect(page).to_not have_content("Manager")
-      expect(page).to_not have_content("Admin")
+    end
+    within(".user-#{user_1.id}") do
+      within(".role") {expect(page).to have_content("member")}
+      within(".role") {expect(page).to_not have_content("manager")}
+      within(".role") {expect(page).to_not have_content("admin")}
     end
   end
 end
