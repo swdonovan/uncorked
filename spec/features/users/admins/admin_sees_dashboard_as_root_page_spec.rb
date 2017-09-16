4fb143a470b2dbf43fbba1605fed6c473b5a8b29
Ellen Cooper <ellencooper74@gmail.com>
2017-09-16 12:39:52 -0600

    creates admin dashboard view as root for admin

--- spec/features/users/admins/admin_sees_dashboard_as_root_page_spec.rb
+++ spec/features/users/admins/admin_sees_dashboard_as_root_page_spec.rb
@@ -8,7 +8,10 @@ RSpec.feature "Admin visits root page" do
     visit '/'
     expect(current_path).to eq('/admin/dashboard')
     expect(page).to have_content("Admin Dashboard")
-    
+    expect(page).to have_content("Manage Users")
+    expect(page).to have_content("Manage Wines")
+    expect(page).to have_content("Manage Venues")
+
     click_on "Uncorked"
     expect(current_path).to eq('/admin/dashboard')
     expect(page).to have_content("Admin Dashboard")
