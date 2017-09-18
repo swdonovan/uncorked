bf783d7bf0db79597c611bb15dc5b8bd2347afb9
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-18 14:12:06 -0600

    updates wine show test with API info

--- spec/features/wines/user_can_view_one_wine_spec.rb
+++ spec/features/wines/user_can_view_one_wine_spec.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
 RSpec.feature 'User can see one wine' do
   context 'without being logged in' do
     it 'guest can view a single wine\'s details' do
-      wine = create(:wine, code: "guigal-cotes-rouge-2010", name: "Guigal Cotes Rouge")
+      wine = create(:wine, code: "olmaia-cabernet-di-toscana", name: "Olmaia Cabernet di Toscana")
       visit wines_path
       click_on wine.name
 
       expect(current_path).to eq wine_path(wine.code)
       expect(page).to have_content(wine.name)
-      expect(page).to have_content("Varietal: Grenache^mourvedre^Syrah")
-      expect(page).to have_content("Vintage: 2010")
-      expect(page).to have_content("Vineyard: E. Guigal")
+      expect(page).to have_content("Vintage: ")
+      expect(page).to have_content("Varietal: Cabernet Sauvignon")
+      expect(page).to have_content("Vineyard: Col D Orcia")
+      expect(page).to have_content("Varietal: Cabernet Sauvignon")
       expect(page).to have_content("Rating: ")
       expect(page).to have_content("Price: ")
     end
